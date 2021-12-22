19ae61fcd70bfe643c1eea92392513ea56907379
Patrick Schleizer <adrelanos@whonix.org>
2021-12-22 16:09:54 -0500

    .

--- LinkToArchive.php
+++ LinkToArchive.php
@@ -6,33 +6,51 @@ use Html;
 
 class LinkToArchive
 {
-    /**
-     * @param $url
-     * @param $text
-     * @param $link
-     * @param array $attribs
-     * @param $linktype
-     * @return bool
-     */
-    public static function onLinkerMakeExternalLink($url, $text, &$link, array &$attribs, $linktype)
-    {
-        if ($linktype
-            && in_array(parse_url($url, PHP_URL_SCHEME), ['http', 'https'])
-        ) {
-            $attribs['href'] = $url;
-            $archiveAttribs = [
-                'rel' => $attribs['rel'],
-                'href' => 'https://web.archive.org/web/' . $url,
-            ];
-            if (isset($attribs['target'])) {
-                $archiveAttribs['target'] = $attribs['target'];
-            }
-            $link = Html::rawElement('a', $attribs, $text) . ' <sup class="ext-link-to-archive">' . Html::rawElement('a', $archiveAttribs, '[' . wfMessage('archive')->parse() . ']') . '</sup>';
+  /**
+  * @param $url
+  * @param $text
+  * @param $link
+  * @param array $attribs
+  * @param $linktype
+  * @return bool
+  */
+  public static function onLinkerMakeExternalLink($url, $text, &$link, array &$attribs, $linktype) {
+    if ($linktype && in_array(parse_url($url, PHP_URL_SCHEME), ['http', 'https']) ) {
+      $attribs['href'] = $url;
 
-            //We need to return false if we want to modify the HTML of external links
-            return false;
-        }
+      // Analysing if it's already an archive link or .onion link
+      if( preg_match( '/\.onion$/', $url ) ) $linkVariant = 'onion';
+      else if( preg_match( '/^https:\/\/web\.archive\.org\/web\//', $url ) ) $linkVariant = 'archive';
+      else $linkVariant = 'normal';
 
-        return null;
+      // Collection attributes for new link, url unchanged if archive link or .onion-link
+      $archiveAttribs = [
+        'rel' => $attribs['rel'],
+        'href' => ( $linkVariant == 'normal' ? 'https://web.archive.org/web/' : '' ) . $url,
+      ];
+      if (isset($attribs['target'])) {
+        $archiveAttribs['target'] = $attribs['target'];
+      }
+
+      // Create new link construct, starting with normal link
+      $link = Html::rawElement('a', $attribs, $text) . '<sup class="ext-link-to-archive" title="';
+
+      // title attribute according to link variant
+      if( $linkVariant == 'onion' ) $link .= 'This is an .onion-link';
+      else if( $linkVariant == 'archive' ) $link .= 'This is a web.archive.org-link';
+      else $link .= 'Link to archived version';
+
+      // image and url of the small link according to link variant
+      $link .= '">&nbsp; '
+        . Html::rawElement('a', $archiveAttribs, '<img src="/w/images/'
+          . ( $linkVariant == 'onion' ? 'thumb/a/a8/Iconfinder_tor_386502.png/40px-Iconfinder_tor_386502.png' : '7/73/Internet_Archive_logo.png' )
+        . '" />' )
+        . '</sup>';
+
+      // We need to return false if we want to modify the HTML of external links
+      return false;
     }
+
+    return null;
+  }
 }
