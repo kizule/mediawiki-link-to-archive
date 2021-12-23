4092ec4e1d8253e798fd0520807d99e0c0c8d950
Patrick Schleizer <adrelanos@whonix.org>
2021-12-23 11:36:13 -0500

    .

--- LinkToArchive.php
+++ LinkToArchive.php
@@ -19,8 +19,8 @@ class LinkToArchive
       $attribs['href'] = $url;
 
       // Analysing if it's already an archive link or .onion link
-      if( preg_match( '/\.onion$/', $url ) ) $linkVariant = 'onion';
-      else if( preg_match( '/^https:\/\/web\.archive\.org\/web\//', $url ) ) $linkVariant = 'archive';
+      if( preg_match( '/^https?:\/\/[^\.]+\.?[^\.]+\.onion/', $url ) ) $linkVariant = 'onion';
+      else if( preg_match( '/^https:\/\/web\.archive\.org\/web/', $url ) ) $linkVariant = 'archive';
       else $linkVariant = 'normal';
 
       // Collection attributes for new link, url unchanged if archive link or .onion-link
@@ -36,12 +36,12 @@ class LinkToArchive
       $link = Html::rawElement('a', $attribs, $text) . '<sup class="ext-link-to-archive" title="';
 
       // title attribute according to link variant
-      if( $linkVariant == 'onion' ) $link .= 'This is an .onion-link';
-      else if( $linkVariant == 'archive' ) $link .= 'This is a web.archive.org-link';
+      if( $linkVariant == 'onion' ) $link .= 'This is an .onion link';
+      else if( $linkVariant == 'archive' ) $link .= 'This is a web.archive.org link';
       else $link .= 'Link to archived version';
 
       // image and url of the small link according to link variant
-      $link .= '">&nbsp; '
+      $link .= '">'
         . Html::rawElement('a', $archiveAttribs, '<img src="/w/images/'
           . ( $linkVariant == 'onion' ? 'thumb/a/a8/Iconfinder_tor_386502.png/40px-Iconfinder_tor_386502.png' : '7/73/Internet_Archive_logo.png' )
         . '" />' )
